d1da40dcaaed1169b94331957f0974665f42c6fa
Yu Yaoshen <yaoshen.yu@outlook.com>
2025-08-03 19:24:00 +0800

    修复代码隐名化功能

--- astensEncoder/src/codeNormalizer.java
+++ astensEncoder/src/codeNormalizer.java
@@ -6,7 +6,7 @@ public class codeNormalizer {
 	public static void main(String args[]) throws IOException {
 		String currentPath = "test/";
 		Boolean commentsDeletion = true;
-		Boolean codeNormalization = true;
+		Boolean codeNormalization = false;
 
 		String srcPath = currentPath + "/source/";
 		String writePath = currentPath + "/normalization/";
